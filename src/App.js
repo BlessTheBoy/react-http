615dbbf36578eed767251fdeb8459ff141d550de
Bakare Faruq <bakarefaruq.a@gmail.com>
2020-10-30 19:54:36 -0700

    HookPractise(6)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounter2 from './components/HookCounter2';
 
 function App() {
   return (
     <div className="App">
-      <PostForm />
+      <HookCounter2 />
+      {/* <HookCounter /> */}
+      {/* <ClassCounter /> */}
+      {/* <PostForm /> */}
       {/* <PostList /> */}
     </div>
   );
